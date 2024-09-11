6d1fad3195b4f1bc44bee6b3dc194df15b854816
Gonzalo Diaz <devel@gon.cl>
2024-09-11 18:56:31 -0300

    [Hacker Rank]: Warmup: A Very Big Sum. Solved ✅.

---
+++ src/lib/exercises/src/hackerrank/warmup/a_very_big_sum.cpp
@@ -0,0 +1,17 @@
+#include <exercises/hackerrank/warmup/a_very_big_sum.hpp>
+
+/**
+ * @link Problem definition [[docs/hackerrank/warmup/a_very_big_sum.md]]
+ */
+
+#include <numeric>
+#include <vector>
+
+namespace hackerrank::warmup {
+
+    long aVeryBigSum(const std::vector<long>& ar) {
+        const long INIT_VALUE = 0L;
+        return std::accumulate(ar.begin(), ar.end(), INIT_VALUE);
+    }
+
+}
