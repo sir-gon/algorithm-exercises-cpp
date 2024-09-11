ee541253b89a589a1219ceceaafc8c5f921971a5
Gonzalo Diaz <devel@gon.cl>
2024-09-11 20:35:52 -0300

    [Hacker Rank]: Warmup: Birthday Cake Candles solved ✅

---
+++ src/lib/exercises/include/exercises/hackerrank/warmup/birthday_cake_candles.hpp
@@ -0,0 +1,7 @@
+#include <vector>
+
+#pragma once
+
+namespace hackerrank::warmup {
+    int birthdayCakeCandles(const std::vector<int>& candles);
+}
---
+++ src/lib/exercises/src/hackerrank/warmup/birthday_cake_candles.cpp
@@ -0,0 +1,27 @@
+#include <exercises/hackerrank/warmup/a_very_big_sum.hpp>
+
+/**
+ * @link Problem definition [[docs/hackerrank/warmup/birthday_cake_candles.md]]
+ */
+
+#include <numeric>
+#include <vector>
+
+namespace hackerrank::warmup {
+    int birthdayCakeCandles(const std::vector<int>& candles) {
+        int counter = 0;
+        int maximum = candles[0];
+
+        for (const int& element : candles) {
+            if (element == maximum) {
+                counter += 1;
+            }
+            if (element > maximum) {
+                maximum = element;
+                counter = 1;
+            }
+        }
+
+        return counter;
+    }
+}
