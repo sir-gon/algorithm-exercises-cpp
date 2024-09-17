584340f8f3c7868706df9e05b0c805ebe982bc7d
Gonzalo Diaz <devel@gon.cl>
2024-09-16 21:38:20 -0300

    [Hacker Rank] Warmup: Time Conversion solved ✅

---
+++ src/lib/exercises/include/exercises/hackerrank/warmup/time_conversion.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string>
+
+namespace hackerrank::warmup {
+
+std::string firstN(const std::string_view &input, unsigned long n);
+std::string lastN(const std::string_view &input, unsigned long n);
+
+std::string timeConversion(const std::string &s);
+
+} // namespace hackerrank::warmup
