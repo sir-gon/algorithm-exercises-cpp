06f01729b9d1d2173d533d3bae82c390fff2a114
Gonzalo Diaz <devel@gon.cl>
2024-09-25 00:13:43 -0300

    BASIC:  show limits of some types.

---
+++ src/lib/exercises/include/exercises/basic/limits.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace basic {
+
+void limits();
+
+}
---
+++ src/tests/unit/lib/basic/limits.test.cpp
@@ -0,0 +1,5 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include <exercises/basic/limits.hpp>
+
+TEST_CASE("limits", "[basic]") { basic::limits(); }
